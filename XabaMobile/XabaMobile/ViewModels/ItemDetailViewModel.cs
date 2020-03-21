2a2b05fd267f435fd766895ea8be8795427e82e4
ideic_azure <ideic75@gmail.com>
2020-03-21 15:27:25 +0100

    init

---
+++ XabaMobile/XabaMobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XabaMobile.Models;
+
+namespace XabaMobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
